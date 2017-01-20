cfaead1fc2af53f303f5a447afc929d0e35a4569
Nathan Kau <nathan@DN51qg3m.SUNet>
2017-01-19 23:49:04 -0800

    post-broken vesc. still bricks the vesc

--- conf_general.h
+++ conf_general.h
@@ -108,12 +108,12 @@
  * Servo output driver
  */
 #ifndef SERVO_OUT_ENABLE
-#define SERVO_OUT_ENABLE		1		// Enable servo output
+#define SERVO_OUT_ENABLE		0		// Enable servo output
 #endif
 #define SERVO_OUT_SIMPLE		1		// Use simple HW-based driver (recommended)
 #define SERVO_OUT_PULSE_MIN_US	1000	// Minimum pulse length in microseconds
 #define SERVO_OUT_PULSE_MAX_US	2000	// Maximum pulse length in microseconds
-#define SERVO_OUT_RATE_HZ		50		// Update rate in Hz
+#define SERVO_OUT_RATE_HZ		400		// Update rate in Hz
 
 // Correction factor for computations that depend on the old resistor division factor
 #define VDIV_CORR			((VIN_R2 / (VIN_R2 + VIN_R1)) / (2.2 / (2.2 + 33.0)))
